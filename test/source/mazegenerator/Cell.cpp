058ef08e3f6560da49ff4fdabfd610a3d73efef3
Daniel Lawrence <danlaw@amazon.com>
2021-11-21 10:35:11 -0600

    Remove iterator assertions in cells test

--- test/source/mazegenerator/Cell.cpp
+++ test/source/mazegenerator/Cell.cpp
@@ -32,10 +32,6 @@ TEST_CASE("Cell") {
     cell1->link(cell2, true);
     CHECK(cell1->linkedTo(cell2));
     CHECK(cell2->linkedTo(cell1));
-    CHECK(std::find(cell1->getLinks().begin(), cell1->getLinks().end(), cell2)
-          != cell1->getLinks().end());
-    CHECK(std::find(cell2->getLinks().begin(), cell2->getLinks().end(), cell1)
-          != cell2->getLinks().end());
   }
   SUBCASE("A cell can list its neighbors") {
     auto cell = Cell::create(0, 0);
